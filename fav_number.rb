24b97dbbcd89f5dfc4c41e060bceb052fdd0fc03
sophiachapman <49126148+sophiachapman@users.noreply.github.com>
2019-07-01 12:29:53 -0500

    Add file
    see what it thinks about your favorite number

---
+++ fav_number.rb
@@ -0,0 +1,4 @@
+puts 'What is your favorite number?'
+number = gets.chomp
+n_number = number.to_i + 1
+puts 'How mediocre. May I suggest ' + n_number.to_s + '? It is a bigger and better favorite number to have.'
